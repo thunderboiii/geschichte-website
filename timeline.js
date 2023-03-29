d49c06eb4fe129ec5a854ab8354a61ed5e0f6cb4
thunderboiii <emil@maple-ink.com>
2023-03-29 21:58:17 +0200

    Initial Commit

---
+++ timeline.js
@@ -0,0 +1,27 @@
+const time = document.getElementById('time');
+const details = document.getElementById('details');
+
+time.addEventListener('click', () => {
+    details.classList.toggle('show');
+});
+
+const time2 = document.getElementById('time-2');
+const details2 = document.getElementById('details-2');
+
+time2.addEventListener('click', () => {
+    details2.classList.toggle('show');
+});
+
+const time3 = document.getElementById('time-3');
+const details3 = document.getElementById('details-3');
+
+time3.addEventListener('click', () => {
+    details3.classList.toggle('show');
+});
+
+const time4 = document.getElementById('time-4');
+const details4 = document.getElementById('details-4');
+
+time4.addEventListener('click', () => {
+    details4.classList.toggle('show');
+});
